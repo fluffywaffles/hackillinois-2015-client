4db72f1872fa76d6eaadfc0f90a37fed75335b4a
skorlir <skorlir@gmail.com>
2015-02-28 15:50:43 -0600

    start using  to append directives to base directive

---
+++ app/scripts/angular/directives/jig-edit.js
@@ -0,0 +1,6 @@
+angular.module('jig')
+.directive('jigEdit', function () {
+  return {
+
+  };
+});
--- app/scripts/angular/directives/jig-text.js
+++ app/scripts/angular/directives/jig-text.js
@@ -4,8 +4,8 @@ angular.module('jig')
     transclude: true,
     template: '<ng-transclude>',
     link: function($scope, $element, $attrs) {
-      $element.append($compile('<jig-rig>')($scope));
-      $element.append($compile('<jig-edit>')($scope));
+      $element.append($compile('<jig-rig>', $attrs)($scope));
+      $element.append($compile('<jig-edit>', $attrs)($scope));
     }
   };
 }]);
